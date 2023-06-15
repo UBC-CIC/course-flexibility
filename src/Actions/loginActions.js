ccbbd4297ef271cb81c57aecac83423faf0878e8
Visal Saosuo <vsaosuo@dhcp-128-189-156-61.ubcsecure.wireless.ubc.ca>
2023-06-15 16:15:28 -0700

    Added frontend

--- src/Actions/loginActions.js
+++
@@ -1,7 +0,0 @@
-// ===================================---CHANGE LOGIN STATE---=======================================
-// Updates the login state of the application
-export const updateLoginState = (payload) => {
-  return (dispatch) => {
-    dispatch({ type: "SET_LOGIN_STATE", payload: payload });
-  };
-};
