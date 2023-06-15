da5ba0fbe9da83d659f210f577ba40a8355bfe39
Tien Nguyen <nhantien1707@gmail.com>
2023-06-15 16:35:42 -0700

    successfully deployed dataworkflow stack

--- backend/cdk/lambda/gqlResolverLambda/gqlResolverLambda.js
+++ backend/cdk/lambda/gqlResolverLambda/gqlResolverLambda.js
@@ -47,8 +47,10 @@ exports.handler = async (event, context) => {
       /** Mutation Operation **/
       case "startJobRun":
         let new_guideline = event.arguments.guideline
-        let date = new Date()
-        date = date.toISOString().split('T')[0]
+        let current_date = new Date()
+        let date = current_date.toISOString().split('T')[0]
+        let unix_timestamp = current_date.getTime()
+        let combined_timestamp = date + '-' + unix_timestamp
         console.log("mutation: Start Job Run");
         const params = {
           //JobName: "preload_database_with_SQL",
@@ -56,7 +58,7 @@ exports.handler = async (event, context) => {
           Arguments: {
             "--NEW_GUIDELINE": new_guideline,
             "--INVOKE_MODE": "new_guideline",
-            "--TIMESTAMP": date
+            "--TIMESTAMP": combined_timestamp
           },
         };
         try {
@@ -73,6 +75,7 @@ exports.handler = async (event, context) => {
           }
         }
         
+        
         break;
         
       case "addGuideline":
@@ -85,7 +88,7 @@ exports.handler = async (event, context) => {
         
       case "removeGuideline":
         const guideline_id = JSON.parse(event.arguments.guidelineID);
-        for(var i = 0; i < guideline_id.length; i++){
+        for(let i = 0; i < guideline_id.length; i++){
           await sql`DELETE FROM flexibility_guideline WHERE guideline_id = ${guideline_id[i]};`; 
         }
         
@@ -110,7 +113,7 @@ exports.handler = async (event, context) => {
         var rows = [];
         
         // Loop though each syllabus
-        for(var sb_i in dbRespond){
+        for(let sb_i in dbRespond){
           const sb = dbRespond[sb_i];
           
           // Keep analysis fetching data
@@ -118,7 +121,7 @@ exports.handler = async (event, context) => {
             const as = await sql`SELECT * FROM analysis_result WHERE syllabus_id = ${sb.syllabus_id}`;
             
             var analysisResult = [];
-            for(var an in as){
+            for(let an in as){
                 var analysis = {
                     guideline_id: as[an].guideline_id,
                     result_txt: as[an].output_result,
@@ -156,45 +159,140 @@ exports.handler = async (event, context) => {
         response.result = JSON.stringify(dbRespond);
         
         break;
-      case "getTodo":
-        dbRespond = await sql`SELECT * FROM flexibility_guideline`;
-        response.result = JSON.stringify(dbRespond);
-        
-        break;
         
       case "getFacultyResult":
-        dbRespond = await sql`SELECT faculty, campus, date_uploaded, 
-                                COUNT(*) AS course_count,
-                                COUNT(CASE WHEN analyzed = false THEN 1 END) AS unanalyzed_count
+        dbRespond = await sql`SELECT faculty, campus, array_agg(DISTINCT date_uploaded) AS date_uploaded, 
+                                COUNT(*) AS course_count
                                 FROM syllabus
-                                GROUP BY faculty, campus, date_uploaded
-                                ORDER BY faculty, campus, date_uploaded;
+                                GROUP BY faculty, campus 
+                                ORDER BY faculty, campus;
                                 `;
         response.result = JSON.stringify(dbRespond);
         
         break; 
       
       case "getCampusResult":
-        dbRespond = await sql`SELECT campus, date_uploaded, 
-                                COUNT(*) AS course_count,
-                                COUNT(CASE WHEN analyzed = false THEN 1 END) AS unanalyzed_count
+        dbRespond = await sql`SELECT campus, array_agg(DISTINCT faculty) AS faculties, array_agg(DISTINCT date_uploaded) AS date_uploaded,
+                                COUNT(*) AS course_count
                                 FROM syllabus
-                                GROUP BY campus, date_uploaded
-                                ORDER BY campus, date_uploaded;
+                                GROUP BY campus;
                                 `;
         response.result = JSON.stringify(dbRespond);
         
         break; 
       
-      case "getFacultyList":
-        dbRespond = await sql`SELECT faculty
-                                FROM syllabus
-                                GROUP BY faculty
-                                ORDER BY faculty;
-                                `;
-        response.result = JSON.stringify(dbRespond);
-        
-        break; 
+      case "getGuidelineCountCampus":
+          let campus = event.arguments.campus;
+
+          let counts = [];
+          
+          // Get all year
+          const year_c = await sql`SELECT DISTINCT date_uploaded FROM syllabus;`;
+          
+          // Get all guidelines
+          let guidelines = await sql`SELECT guideline FROM flexibility_guideline`;
+          
+          
+          
+          for(let c in year_c){
+            const year_i = year_c[c].date_uploaded;
+            
+            // Get total number of syllabus
+            const syllSize = await sql`SELECT COUNT(*) FROM syllabus as s 
+                                        WHERE s.date_uploaded = ${year_i} AND s.campus = ${campus}`;
+            let guidelineCount = [];
+            
+            for(let i in guidelines){
+              const gl = guidelines[i].guideline;
+              
+              // Get guidelines percentage
+              const req = await sql`SELECT (
+                                        SELECT COUNT(*) FROM analysis_result AS a
+                                        INNER JOIN syllabus AS s ON a.syllabus_id = s.syllabus_id
+                                        INNER JOIN flexibility_guideline AS g ON a.guideline_id = g.guideline_id
+                                        WHERE s.date_uploaded = ${year_i}
+                                            AND s.campus = ${campus}
+                                            AND a.output_result = 'yes' 
+                                            AND g.guideline = ${gl}
+                                    ) AS result;`;
+              guidelineCount.push({
+                guideline: gl,
+                count: (Number(req[0].result)/Number(syllSize[0].count))*100,
+              });
+            }
+            
+            // Load result based on year
+            counts.push({
+              year: year_i,
+              result: guidelineCount
+            });
+          }
+          
+          
+          response.result = JSON.stringify(counts);
+          
+          break;
+          
+          
+                                
+      case "getGuidelineCountFaculty":
+          const faculty = event.arguments.faculty;
+          // const year_f = event.arguments.year;
+          
+          let counts_f = [];
+          
+          // Get all year
+          const year_f = await sql`SELECT DISTINCT date_uploaded FROM syllabus;`;
+          
+          // Get all guidelines
+          const guidelines_f = await sql`SELECT guideline FROM flexibility_guideline`;
+          
+          for(let y in year_f){
+            const year_i = year_f[y].date_uploaded;
+            
+            // Get total number of syllabus
+            let syllSize_i = await sql`SELECT COUNT(*) FROM syllabus as s 
+                                        WHERE s.date_uploaded = ${year_i} AND s.faculty = ${faculty}`;
+            let guidelineCount = [];
+            for(let i in guidelines_f){
+              const gl = guidelines_f[i].guideline;
+              
+              // Get guidelines percentage
+              const req = await sql`SELECT (
+                                        SELECT COUNT(*) FROM analysis_result AS a
+                                        INNER JOIN syllabus AS s ON a.syllabus_id = s.syllabus_id
+                                        INNER JOIN flexibility_guideline AS g ON a.guideline_id = g.guideline_id
+                                        WHERE s.date_uploaded = ${year_i}
+                                            AND s.faculty = ${faculty}
+                                            AND a.output_result = 'yes' 
+                                            AND g.guideline = ${gl}
+                                    ) AS result;`;
+              guidelineCount.push({
+                guideline: gl,
+                count: (Number(req[0].result)/Number(syllSize_i[0].count))*100,
+              });
+            }
+            
+            // Load result based on year
+            counts_f.push({
+              year: year_i,
+              result: guidelineCount
+            });
+          }
+          
+          response.result = JSON.stringify(counts_f);
+          
+          break;
+                                
+        case "getFacultyList":
+          dbRespond = await sql`SELECT faculty
+                                  FROM syllabus
+                                  GROUP BY faculty
+                                  ORDER BY faculty;
+                                  `;
+          response.result = JSON.stringify(dbRespond);
+          
+          break; 
         
       /** Test Operations **/
       case "loadSQL":
